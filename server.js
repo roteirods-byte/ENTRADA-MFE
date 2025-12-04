d81fc41c3732f5c4a2fabbcdba3cec088f683720
JORGE SOBRINHO <roteiro.ds@gmail.com>
2025-12-04 14:14:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8082;
 
 // ---------- DADOS DE EXEMPLO (mesmos do index.html) ----------
 const dadosExemplo = [
